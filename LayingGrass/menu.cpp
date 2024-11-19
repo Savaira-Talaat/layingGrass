0e3d75719eec8bd136df1bd7f955fb89fb0c7bfc
Savaira <savaira.talaat@gmail.com>
2024-11-19 09:40:13 +0100

    update: add some modifs to menu

--- LayingGrass/menu.cpp
+++ LayingGrass/menu.cpp
@@ -4,21 +4,21 @@
 
 void afficherRegles()
 {
-    std::cout << "\nRègles du jeu\n";
-    std::cout << "1. Objectif : Créer le plus grand territoire carré sur une grille.\n";
+    std::cout << "\nRegles du jeu\n";
+    std::cout << "1. Objectif : Creer le plus grand territoire carre sur une grille.\n";
     std::cout << "2. Joueurs : 2 à 9 joueurs.\n";
     std::cout << "   - 2 à 4 joueurs : grille de 20x20.\n";
     std::cout << "   - 5 à 9 joueurs : grille de 30x30.\n";
-    std::cout << "3. Déroulement :\n";
-    std::cout << "   - Chaque joueur commence avec une tuile 1x1 placée sur la grille.\n";
+    std::cout << "3. Deroulement :\n";
+    std::cout << "   - Chaque joueur commence avec une tuile 1x1 placee sur la grille.\n";
     std::cout << "   - Les joueurs placent des tuiles en les connectant à leur territoire.\n";
     std::cout << "   - Les tuiles ne peuvent pas toucher les territoires des autres joueurs.\n";
-    std::cout << "   - Les joueurs disposent d'un coupon pour échanger une tuile si besoin.\n";
+    std::cout << "   - Les joueurs disposent d'un coupon pour echanger une tuile si besoin.\n";
     std::cout << "4. Bonus :\n";
-    std::cout << "   - Cases spéciales sur la grille offrant des avantages : échange, pierre, vol.\n";
+    std::cout << "   - Cases speciales sur la grille offrant des avantages : echange, pierre, vol.\n";
     std::cout << "5. Fin du jeu :\n";
-    std::cout << "   - Après 9 tours, le joueur ayant le plus grand carré gagne.\n";
-    std::cout << "   - En cas d'égalité, le nombre total de cases de gazon est pris en compte.\n";
+    std::cout << "   - Apres 9 tours, le joueur ayant le plus grand carre gagne.\n";
+    std::cout << "   - En cas d'egalite, le nombre total de cases de gazon est pris en compte.\n";
 }
 
 void commencerPartie()
@@ -33,7 +33,7 @@ int main()
     do
     {
         std::cout << "\nMenu principal:\n";
-        std::cout << "1. Afficher les règles\n";
+        std::cout << "1. Afficher les regles\n";
         std::cout << "2. Commencer une partie\n";
         std::cout << "0. Quitter\n";
         std::cout << "Faites votre choix : ";
